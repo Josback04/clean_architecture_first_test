2acdaf12de08e2740a5b1fe667e955d95aa20394
Joseph Backelant's <backelantsjoseph@gmail.com>
2024-01-30 16:55:35 +0100

    réglage bug, par rappot à article

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:clena_architecture_test/config/theme/app_themes.dart';
+import 'package:clena_architecture_test/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:clena_architecture_test/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
 import 'package:clena_architecture_test/features/daily_news/presentation/pages/daily_news.dart';
 import 'package:clena_architecture_test/injection_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   await initializeDependencies();
@@ -14,10 +17,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: theme(),
-      home: const DailyNews(),
+    return BlocProvider<RemoteArticlesBloc>(
+      create: (BuildContext context) => sl()..add(GetArticles()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: theme(),
+        home: const DailyNews(),
+      ),
     );
   }
 }
